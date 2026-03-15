d0691332580c9120887ce3d71f370354082301d6
Rajath Kiran A <arajath5463@gmail.com>
2026-03-15 11:45:22 +0530

    Add comments to path.extname and path.basename methods

--- 03_path_module_demo.js
+++ 03_path_module_demo.js
@@ -4,3 +4,25 @@
 // Require the 'path' module to start using it
 const path = require('path');
 
+
+// 1. path.extname()
+// Gets the extension of the file from a given path (e.g., '.txt', '.js').
+let ext = path.extname('D:\\Creative\\Scaler-Nodejs\\sample_text_file.txt');
+
+// 2. path.basename()
+// Gets the actual name of the file from a given path (e.g., 'sample_text_file.txt').
+let basename = path.basename('D:\\Creative\\Scaler-Nodejs\\sample_text_file.txt');
+
+// Print the extracted file extension
+console.log(ext);
+
+// Print the extracted file basename
+console.log(basename);
+
+// 3. __filename
+// A built-in Node.js variable that holds the absolute path of the current file being executed.
+console.log(__filename);
+
+// 4. __dirname
+// A built-in Node.js variable that holds the absolute path of the directory containing the current file.
+console.log(__dirname);
